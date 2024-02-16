c8184e787784b3361d110b60728c93bb00cb8d12
Dan Orjuela <dancid84@hotmail.com>
2024-02-16 01:40:06 -0500

    Initial Commit

---
+++ 001 - Hello/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  001 - Hello
+//
+//  Created by Daniel Orjuela on 16/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
